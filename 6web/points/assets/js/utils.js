433e9c46b6a1d1bfa8dcac3b64eadd60df88b55a
tangzheng <tangzheng0105@gmail.com>
2018-07-10 22:38:20 +0800

    修改列表标题过长重叠

--- 6web/points/assets/js/utils.js
+++ 6web/points/assets/js/utils.js
@@ -4,17 +4,40 @@ function GetQueryString(name) {
     if (r != null) return decodeURIComponent(r[2]); return null;
 };
 
-function myRequest(url, data, callback){
+function myRequest(url, data, callback) {
     // var host = window.location.origin
     // var host = "http://192.168.1.100:89";
     var host = "http://47.95.220.122:90";
     var setting = {}
     data.session = GetQueryString('session') || $.fn.cookie('session')
-    setting.url = host+url;
+    setting.url = host + url;
     setting.type = "POST";
     setting.data = data;
-    setting.success = function(res) {
+    setting.success = function (res) {
         callback(res)
     }
     $.ajax(setting)
+}
+
+/*这段代码是固定的，必须要放到js中*/
+function setupWebViewJavascriptBridge(callback) {
+    if (window.WebViewJavascriptBridge) {
+        callback(WebViewJavascriptBridge)
+    } else {
+        document.addEventListener(
+            'WebViewJavascriptBridgeReady'
+            , function () {
+                callback(WebViewJavascriptBridge)
+            },
+            false
+        );
+    }
+
+    if (window.WVJBCallbacks) { return window.WVJBCallbacks.push(callback); }
+    window.WVJBCallbacks = [callback];
+    var WVJBIframe = document.createElement('iframe');
+    WVJBIframe.style.display = 'none';
+    WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__';
+    document.documentElement.appendChild(WVJBIframe);
+    setTimeout(function () { document.documentElement.removeChild(WVJBIframe) }, 0)
 }
